6e333dede421cfbe22e55f708a5cc29ad2d65f22
Vladyslav Sheiko <vladislav.sheyko@hotmail.com>
2025-03-12 13:07:03 +0200

    fix: added public url for github actions

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // or whatever plugin you're using
+
+export default defineConfig({
+  base: '/personal-website/', // This should match your repository name
+})

46d60864f8c44f466231dda7d2e952ee2c4781cd
Vladyslav Sheiko <vladislav.sheyko@hotmail.com>
2025-03-12 13:11:28 +0200

    fix: updated vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or whatever plugin you're using
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/personal-website/', // This should match your repository name
+  plugins: [react()],
+  base: '/personal-website/',
+  
+  resolve: {
+    alias: {
+      'react': 'react',
+      'react-dom': 'react-dom',
+    }
+  },
+  
+  esbuild: {
+    jsxInject: `import React from 'react'`, // This automatically injects React into JSX files
+  }
 })
